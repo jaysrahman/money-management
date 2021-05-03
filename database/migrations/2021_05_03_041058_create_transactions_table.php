42e80573ae21e1f394f48a6599c2e841a4166484
jaysrahman <jaisjezz41@gmail.com>
2021-05-03 12:51:12 +0700

    Added Transactions and Transaction Types Table

---
+++ database/migrations/2021_05_03_041058_create_transactions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('date');
+            $table->integer('nominal');
+            $table->longText('description');
+            $table->integer('ending_balance');
+            $table->unsignedBigInteger('transaction_type_id');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
